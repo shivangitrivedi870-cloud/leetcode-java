8c84adeac078febb334c36f5867523e3223f4622
shivangitrivedi870-cloud <shivangitrivedi870@gmail.com>
2026-07-08 19:57:59 +0530

    Time: 1 ms (97.62%) | Memory: 42.4 MB (78.34%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if ( x==0) return 0;
+
+    int s = 1;
+    int e = x;
+    int ans = -1;
+
+    while ( s <= e) {
+        int mid = s + (e-s)/2;
+
+        if(mid == x/mid) {
+            return mid;
+        }
+        else if (mid > x/mid) {
+
+            e = mid - 1;
+         
+        }
+        else {
+            ans = mid;
+            s = mid + 1;
+        }
+    }
+    return ans;
+}
+}
